7f430b802bf5488cf654d04604fb2bc68bf513b4
David Murphy <dave.w.murphy@gmail.com>
2025-04-24 10:46:59 -0700

    Remove unused vars

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,19 +1,16 @@
-import { reactRouter } from '@react-router/dev/vite';
-import { cloudflare } from '@cloudflare/vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
-
-const MODE = process.env.NODE_ENV;
+import { reactRouter } from "@react-router/dev/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ isSsrBuild }) => ({
-    plugins: [
-        cloudflare({
-            viteEnvironment: { name: 'ssr' },
-        }),
-        tailwindcss(),
-        reactRouter(),
-        tsconfigPaths(),
-    ],
+  plugins: [
+    cloudflare({
+      viteEnvironment: { name: "ssr" },
+    }),
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 }));
